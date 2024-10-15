e541331f9f539f2da3376994cfa83f96ab4d05e8
Chikakoo <lionel.crafts@gmail.com>
2024-10-15 08:46:43 -0500

    Create a common component for loggers an split it into a spoiler and bundle loggers; still need to add functionality to the bundle logger; added bundle logger setting

---
+++ More Random Edition/Loggers/BundleLogger.cs
@@ -0,0 +1,34 @@
+﻿namespace Randomizer;
+
+//TODO: initialize this AND actually use it!
+/// <summary>
+/// Spoiler-free log of all bundles for easy tracking
+/// </summary>
+public class BundleLogger : Logger
+{
+	/// <summary>
+	/// Constructor
+	/// </summary>
+	/// <param name="farmName">The name of the farm - used to easily identify the log</param>
+	public BundleLogger(string farmName)
+	{
+		if (!Globals.Config.CreateBundleLog) { return; }
+
+		PathPrefix = "BundleLog";
+		InitializePath(farmName);
+	}
+
+	/// <summary>
+	/// Writes text to the file, provided the settings allow it
+	/// </summary>
+	public override void WriteFile()
+	{
+		if (!Globals.Config.CreateBundleLog)
+		{
+			TextToWrite = "";
+			return;
+		}
+
+		base.WriteFile();
+	}
+}
